067303035d92afceab03ebf9fc4de1922206b609
Manas Singh <manasinghofficial@gmail.com>
2026-03-18 20:00:30 +0530

    style: enhance Header component layout and styling for improved visual hierarchy

--- components/sections/Header.tsx
+++ components/sections/Header.tsx
@@ -22,15 +22,21 @@ const Header = () => {
   return (
     <header className="flex text-text-primary">
       <nav className="flex w-full items-center justify-between">
-        <div className="text-xl lg:text-2xl">MANAS</div>
+        <div className="text-3xl font-bold lg:text-2xl flex items-baseline justify-center gap-0">
+          <span className="leading-none">MS</span>
+          <span className="font-extrabold text-text-secondary font-instrument text-5xl leading-none -mt-1 -ml-1">.</span>
+        </div>
 
         <ul className="hidden items-center gap-15 lg:flex">
           {headerLinks.map(({ label, href, icon }) => (
             <li key={label}>
-               <Link href={href} className="group flex lg:text-md font-semibold text-text-primary transition-colors duration-300 hover:text-white">
+              <Link
+                href={href}
+                className="group flex lg:text-md font-semibold text-text-primary transition-colors duration-300 hover:text-white"
+              >
                 <div className="flex items-center">
                   {label}
-                   <div className="ml rotate-45 transition-transform duration-300 group-hover:rotate-90">
+                  <div className="ml rotate-45 transition-transform duration-300 group-hover:rotate-90">
                     <Image src={icon} alt="" width={18} height={18} />
                   </div>
                 </div>
@@ -45,7 +51,13 @@ const Header = () => {
         </ul>
 
         <div className="lg:hidden">
-          <Image src="/icons/ham.svg" alt="menu" width={30} height={30} className="w-7.5" />
+          <Image
+            src="/icons/ham.svg"
+            alt="menu"
+            width={30}
+            height={30}
+            className="w-7.5"
+          />
         </div>
       </nav>
     </header>
